ffaa63d5d26cb23b45982baa62a3f2315896d9fa
Qammar <qammarsahu@gmail.com>
2023-02-21 03:58:26 +0500

    Singnals.py Addded

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,6 +1,8 @@
 
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path,include
 from users import views as user_views
 
@@ -13,3 +15,9 @@ urlpatterns = [
     path('',include('blog.urls')),
     
 ]
+
+if settings.DEBUG:
+   
+       urlpatterns    += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
