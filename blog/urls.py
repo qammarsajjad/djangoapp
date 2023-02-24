1411a7de1d37af262e95acba5dab51984481326c
Qammar <qammarsahu@gmail.com>
2023-02-24 11:25:02 +0500

    Updated

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import PostListView,PostDetail
+from .views import PostListView,PostDetail,PostCreateView,PostUpdateView,PostDelete
 from . import views
 
 urlpatterns = [
     path('',PostListView.as_view(),name= 'blog-home'),
     path('post-detail/<int:pk>/',PostDetail.as_view(),name= 'post-detail'),
+    path('post-new/',PostCreateView.as_view(),name= 'post-create'),
+    path('post-update/<int:pk>/',PostUpdateView.as_view(),name= 'post-update'),
+    path('post-delete/<int:pk>/',PostDelete.as_view(),name= 'post-delete'),
     path('about/',views.About,name= 'blog-about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView,DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
 from .models import Post
 
 
@@ -17,9 +18,49 @@ class PostListView(ListView):
     
     ordering = ['-date_posted']
 
-class PostDetail(DeleteView):
+class PostDetail(DetailView):
     model = Post
-    template_name = 'blog/post_detail.html'
+ 
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = Post
+    fields=['title','content']
+
+    def form_valid(self,form):
+        form.instance.auther = self.request.user
+        return super().form_valid(form)
+        
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model = Post
+    fields=['title','content']
+
+    def form_valid(self,form):
+        form.instance.auther = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.auther:
+            return True
+        return False
+
+
+class PostDelete(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = Post
+    success_url = '/'
+    template_name = 'blog/post_form_confrim.html'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.auther:
+            return True
+        return False
+   
 
 def About(request):
    return render(request,'blog/about.html',{'title':'About'})
+     
+
+        
+
